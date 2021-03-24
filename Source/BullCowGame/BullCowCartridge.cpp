5c3c5528695f1dbc07147d5d52b4610338b22302
Sudip Mondal <sudipmondal.2002@rediffmail.com>
2021-03-24 21:46:09 +0530

    initial commit

---
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -0,0 +1,87 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "BullCowCartridge.h"
+#include "HiddenWords.h"
+
+void UBullCowCartridge::BeginPlay() // When the game starts
+{
+    Super::BeginPlay();
+    InitGame();
+}
+
+void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
+{
+    if (bGameOver)
+    {
+        ClearScreen();
+        InitGame();
+    }
+    else
+    {
+        ProcessGuess(Input, Lives);
+    }
+}
+
+void UBullCowCartridge::InitGame()
+{
+    HiddenWord = TEXT("cake");
+    Lives = HiddenWord.Len();
+    bGameOver = false;
+    PrintLine(TEXT("Welcome to my Bull-Cow Game"));
+    PrintLine(TEXT("You have %i number lives"), Lives);
+    PrintLine(TEXT("Guess the %i letter word"), HiddenWord.Len());
+    PrintLine(TEXT("Type in your guess"));
+    PrintLine(TEXT("Press enter to continue ...."));
+}
+
+void UBullCowCartridge::EndGame()
+{
+    bGameOver = true;
+    PrintLine(TEXT("\nPress Enter to play again..."));
+}
+
+void UBullCowCartridge::ProcessGuess(FString Guess, int32 Counter)
+{
+    if (Guess == HiddenWord)
+    {
+        PrintLine(TEXT("You have Won!!"));
+        EndGame();
+        return;
+    }
+    if (Guess.Len() != HiddenWord.Len())
+    {
+        PrintLine(TEXT("Length of the character should be %i"), HiddenWord.Len());
+        return;
+    }
+    if (!IsIsogram(Guess))
+    {
+        PrintLine("No repeating letters, try again!!");
+        return;
+    }
+    --Lives;
+    PrintLine("You lost a life");
+    if (Lives > 0)
+    {
+        PrintLine(TEXT("Try Again! You have %i lives left"), Lives);
+    }
+    else
+    {
+        PrintLine(TEXT("Hidden word was: %s"), *HiddenWord);
+        EndGame();
+    }
+}
+
+bool UBullCowCartridge::IsIsogram(FString Word) const
+{
+    for (int32 Index = 0; Index < Word.Len(); Index++)
+    {
+        for (int32 Comparison = Index+1; Comparison < Word.Len(); Comparison++)
+        {
+            if (Word[Index] == Word[Comparison]){
+                return false;
+            }
+        }
+        
+    }
+    
+    return true;
+}
---
+++ Source/BullCowGame/HiddenWords.h
@@ -0,0 +1,8 @@
+#include "CoreMinimal.h"
+const TArray <FString> Words = {
+    TEXT("cakes"),
+    TEXT("bacon"),
+    TEXT("water"),
+    TEXT("might"),
+    TEXT("power")
+}
